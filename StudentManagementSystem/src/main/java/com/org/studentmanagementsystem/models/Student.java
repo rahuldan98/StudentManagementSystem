cdac15194067ea69e8e08ed2db7f2ddd65eed714
Rahul Dan <rahuldan98@gmail.com>
2023-03-03 00:35:04 +0530

    models created

---
+++ StudentManagementSystem/src/main/java/com/org/studentmanagementsystem/models/Student.java
@@ -0,0 +1,62 @@
+package com.org.studentmanagementsystem.models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+
+
+@Entity
+public class Student {
+	@Id
+	private Integer id;
+	private String username;
+	private String password;
+	
+	@OneToMany(fetch= FetchType.LAZY,cascade=CascadeType.ALL,mappedBy = "student")
+	private List<Course>courselist;
+	
+	public Student(Integer id, String username, String password, List<Course> courselist) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.courselist = courselist;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<Course> getCourselist() {
+		return courselist;
+	}
+	public void setCourselist(List<Course> courselist) {
+		this.courselist = courselist;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", username=" + username + ", password=" + password + ", courselist=" + courselist
+				+ "]";
+	}
+	
+	
+}
