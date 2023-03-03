f2bed91c7cf48b957c8274d398636c026f8cbf05
Rahul Dan <rahuldan98@gmail.com>
2023-03-03 22:28:05 +0530

    admin service and repo register

--- StudentManagementSystem/src/main/java/com/org/studentmanagementsystem/service/IAdminService.java
+++ StudentManagementSystem/src/main/java/com/org/studentmanagementsystem/service/IAdminService.java
@@ -1,5 +1,13 @@
 package com.org.studentmanagementsystem.service;
 
-public interface IAdminService {
+import org.springframework.stereotype.Service;
 
+import com.org.studentmanagementsystem.dto.AdminDto;
+import com.org.studentmanagementsystem.models.Admin;
+
+@Service
+public interface IAdminService {
+	public Admin register(AdminDto admindto);
+	public Admin login(AdminDto admindto);
+	
 }
--- StudentManagementSystem/src/main/java/com/org/studentmanagementsystem/service/implementation/AdminServiceImpl.java
+++ StudentManagementSystem/src/main/java/com/org/studentmanagementsystem/service/implementation/AdminServiceImpl.java
@@ -1,7 +1,32 @@
 package com.org.studentmanagementsystem.service.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.org.studentmanagementsystem.dto.AdminDto;
+import com.org.studentmanagementsystem.models.Admin;
+import com.org.studentmanagementsystem.repository.IAdminRepository;
 import com.org.studentmanagementsystem.service.IAdminService;
 
 public class AdminServiceImpl implements IAdminService {
+	private IAdminRepository adminRepository;
+	
+	@Autowired
+	public AdminServiceImpl(IAdminRepository adminRepository) {
+		super();
+		this.adminRepository = adminRepository;
+	}
+
+	@Override
+	public Admin register(AdminDto admindto) {
+		Admin admin;
+		adminRepository.save(admin);
+		return null;
+	}
+
+	@Override
+	public Admin login(AdminDto admindto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
